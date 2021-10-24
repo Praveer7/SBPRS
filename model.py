549f84c964e3fcc78b7ee119ea020c96d572512d
Praveersinh Parmar <praveerparmar@gmail.com>
2021-10-24 22:50:12 +0530

    add all files

--- model.py
+++ model.py
@@ -7,19 +7,19 @@ import pandas as pd
 class Recommendation:
     
     def __init__(self):
-        self.data = pickle.load(open('pickle/processed_data.pkl','rb'))
-        self.user_final_rating = pickle.load(open('pickle/user_final_rating.pkl','rb'))
-        self.model = pickle.load(open('pickle/logistic_regression.pkl','rb'))
-        self.raw_data = pd.read_csv("dataset/sample30.csv")
+        self.data = pd.read_pickle(open('pickle/processed_data.pkl','rb'))
+        self.user_final_rating = pd.read_pickle(open('pickle/user_final_rating.pkl','rb'))
+        self.model = pd.read_pickle(open('pickle/logreg_model.pkl','rb'))
+        self.raw_data = pd.read_csv("data/sample30.csv")
         self.data = pd.concat([self.raw_data[['id','name','brand','categories','manufacturer']],self.data], axis=1)
         
         
     def getTopProducts(self, user):
         items = self.user_final_rating.loc[user].sort_values(ascending=False)[0:20].index
-        features = pickle.load(open('pickle/features.pkl','rb'))
+        features = pickle.load(open('pickle/tfidf_vocab.pkl','rb'))
         vectorizer = TfidfVectorizer(vocabulary = features)
         temp=self.data[self.data.id.isin(items)]
-        X = vectorizer.fit_transform(temp['Review'])
+        X = vectorizer.fit_transform(temp['reviews'])
         temp=temp[['id']]
         temp['prediction'] = self.model.predict(X)
         temp['prediction'] = temp['prediction'].map({'Postive':1,'Negative':0})
